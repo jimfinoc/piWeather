2ec69a1ce7042bdae5f2a4623354065d163c40ca
J <jimfinoc@gmail.com>
2025-02-07 09:19:11 -0800

    weather test

---
+++ weather.py
@@ -0,0 +1,23 @@
+from urllib.request import urlopen 
+import json 
+import gps
+
+  
+# store the response of URL 
+  
+# storing the JSON response  
+# from url in data 
+  
+# print the json response 
+
+if __name__ == "__main__":
+    myGPS = gps.GPS()
+    time.sleep(2)
+    myGPS.update()
+    latitude = myGPS.lat
+    longitude = myGPS.longitude
+    weatherURL = "https://api.weather.gov/points/{latitude},{longitude}"
+    print (weatherURL)
+    response = urlopen(weatherURL) 
+    data_json = json.loads(response.read()) 
+    print(data_json) 
