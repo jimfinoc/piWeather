60f582c28ab2a25671f130d151e31563a2a9126e
J <jimfinoc@gmail.com>
2025-02-06 21:18:35 -0800

    get GPS coordinates using gpsd

---
+++ gps.py
@@ -0,0 +1,41 @@
+class GPS:
+    def __init__(self):
+        from gps3.agps3threaded import AGPS3mechanism
+        # from time import sleep
+        self.agps_thread = AGPS3mechanism()  # Instantiate AGPS3 Mechanisms
+        self.agps_thread.stream_data()  # From localhost (), or other hosts, by example, (host='gps.ddns.net')
+        self.agps_thread.run_thread()  # Throttle time to sleep after an empty lookup, default '()' 0.2 two tenths of a second
+    
+    def update(self):
+        print('---------------------')
+        print('Time:{}  '.format(self.agps_thread.data_stream.time))
+        print('Lat:{}   '.format(self.agps_thread.data_stream.lat))
+        print('Lon:{}   '.format(self.agps_thread.data_stream.lon))
+        print('Speed:{} '.format(self.agps_thread.data_stream.speed))
+        print('Course:{}'.format(self.agps_thread.data_stream.track))
+        print('---------------------')
+    
+    def time(self):
+        return self.agps_thread.data_stream.time
+
+    def lat(self):
+        return self.agps_thread.data_stream.lat
+
+    def lon(self):
+        return self.agps_thread.data_stream.lon
+
+    def status(self):
+        return self.agps_thread.data_stream.status
+
+    def timeOffset(self):
+        return self.agps_thread.data_stream.ept
+
+    def speed(self):
+        return self.agps_thread.data_stream.speed
+
+    def alt(self):
+        return self.agps_thread.data_stream.alt
+
+def __main__():
+    myGPS = GPS()
+    myGPS.update()
