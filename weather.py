67a21615cc13b29fcce3c594b546d03fbb7c7dcf
J <jimfinoc@gmail.com>
2025-02-07 09:30:45 -0800

    lat long

--- weather.py
+++ weather.py
@@ -14,9 +14,11 @@ if __name__ == "__main__":
     myGPS = gps.GPS()
     time.sleep(2)
     myGPS.update()
-    latitude = str(myGPS.lat)
-    longitude = str(myGPS.lon)
-    weatherURL = f"https://api.weather.gov/points/{latitude},{longitude}"
+    weatherURL = "https://api.weather.gov/points/{latitude},{longitude}".format(latitude = myGPS.lat,longitude = myGPS.lon)
+    # 'Number {0}: {1:{2}.2f}'.format(i, num, field_size)
+    # 'Number {i}: {num:{field_size}.2f}'.format(i=i, num=num, field_size=field_size)
+
+
     print (weatherURL)
     response = urlopen(weatherURL) 
     data_json = json.loads(response.read()) 
