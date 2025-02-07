8e31c7c1dca62482e446e0e568e114b94defffb6
J <jimfinoc@gmail.com>
2025-02-07 09:23:16 -0800

    string?

--- weather.py
+++ weather.py
@@ -14,8 +14,8 @@ if __name__ == "__main__":
     myGPS = gps.GPS()
     time.sleep(2)
     myGPS.update()
-    latitude = myGPS.lat
-    longitude = myGPS.lon
+    latitude = str(myGPS.lat)
+    longitude = str(myGPS.lon)
     weatherURL = f"https://api.weather.gov/points/{latitude},{longitude}"
     print (weatherURL)
     response = urlopen(weatherURL) 
