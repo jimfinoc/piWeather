76f8f5a549d47dc37706deb72f0e4a410b763eaa
J <jimfinoc@gmail.com>
2025-02-12 14:35:50 -0800

    1st time

--- weatherRefresh.py
+++ weatherRefresh.py
@@ -1,7 +1,12 @@
 import time
 import weather
+
 if __name__ == "__main__":
     timeHack = time.time()
+    os.system('clear')
+    timeHack = time.time()
+    weather.print_weather()
+
     while True:
         if time.time() - timeHack > 180:
             os.system('clear')
