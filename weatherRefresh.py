758db8b98a5a13103976d2672a4860bd22dea860
J <jimfinoc@gmail.com>
2025-02-12 14:48:12 -0800

    what time is it

--- weatherRefresh.py
+++ weatherRefresh.py
@@ -6,12 +6,16 @@ if __name__ == "__main__":
     timeHack = time.time()
     os.system('clear')
     timeHack = time.time()
+    prettyHack = time.strftime("%m/%d/%Y, %H:%M %Z",time.localtime(timeHack()))
+    print(prettyHack)
     weather.print_weather()
 
     while True:
         if time.time() - timeHack > 180:
             os.system('clear')
             timeHack = time.time()
+            prettyHack = time.strftime("%m/%d/%Y, %H:%M %Z",time.localtime(timeHack()))
+            print(prettyHack)
             weather.print_weather()
 
 
