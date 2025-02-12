3e81f1f3d6d9c7167b5ba9b9b3e5f735839031a7
J <jimfinoc@gmail.com>
2025-02-12 15:01:21 -0800

    add a space

--- weatherRefresh.py
+++ weatherRefresh.py
@@ -6,15 +6,17 @@ if __name__ == "__main__":
     timeHack = time.time()
     os.system('clear')
     timeHack = time.time()
-    prettyHack = time.strftime("%H:%M:%S %Z   \n%a %m %b %Y   ",time.localtime(time.time()))
-    print(prettyHack)
+    prettyNow = time.strftime("%H:%M:%S %Z   \n%a %m %b %Y   ",time.localtime(time.time()))
+    print(prettyNow)
+    print()
     weather.print_weather()
 
     while True:
         time.sleep(1)
         print("\033[1;1H", end="")
-        prettyHack = time.strftime("%H:%M:%S %Z   \n%a %m %b %Y   ",time.localtime(time.time()))
-        print(prettyHack)
+        prettyNow = time.strftime("%H:%M:%S %Z   \n%a %m %b %Y   ",time.localtime(time.time()))
+        print(prettyNow)
+        print()
         if time.time() - timeHack > 180:
             os.system('clear')
             timeHack = time.time()
