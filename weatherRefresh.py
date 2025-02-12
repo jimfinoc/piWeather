02a467699e1e0a3c5d2f57f52e93c886b27b6f96
J <jimfinoc@gmail.com>
2025-02-12 14:57:06 -0800

    no brackets

--- weatherRefresh.py
+++ weatherRefresh.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
 
     while True:
         time.sleep(1)
-        print("\033[<1>;<1>H", end="")
+        print("\033[1;1H", end="")
         prettyHack = time.strftime("%H:%M:%S %Z   \n%a %m %b %Y   ",time.localtime(timeHack))
         if time.time() - timeHack > 180:
             os.system('clear')
