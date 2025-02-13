be962d39cdf43d68e473b921d9bcfce49c03d6be
J <jimfinoc@gmail.com>
2025-02-12 21:12:13 -0800

    clear and move

--- weatherRefresh.py
+++ weatherRefresh.py
@@ -16,6 +16,7 @@ if __name__ == "__main__":
         time.sleep(1)
         if time.time() - timeHack > 180:
             os.system('clear')
+            print()
             prettyNow = time.strftime(" %H:%M:%S %Z   \n %a %m %b %Y   ",time.localtime(time.time()))
             print(prettyNow)
             print()
@@ -23,6 +24,7 @@ if __name__ == "__main__":
             weather.print_weather()
             print("\033[1;1H", end="")
         else:
+            print()
             prettyNow = time.strftime(" %H:%M:%S %Z   \n %a %m %b %Y   ",time.localtime(time.time()))
             print(prettyNow)
             print("\033[1;1H", end="")
