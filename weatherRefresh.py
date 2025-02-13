df84d725953f107c353b895e183392af46e1c2a4
J <jimfinoc@gmail.com>
2025-02-12 21:13:51 -0800

    whats going on

--- weatherRefresh.py
+++ weatherRefresh.py
@@ -5,12 +5,12 @@ import os
 if __name__ == "__main__":
     timeHack = time.time()
     os.system('clear')
-    timeHack = time.time()
+    print()
     prettyNow = time.strftime(" %H:%M:%S %Z   \n %a %m %b %Y   ",time.localtime(time.time()))
     print(prettyNow)
-    print("\033[1;1H", end="")
     print()
     weather.print_weather()
+    print("\033[1;1H", end="")
 
     while True:
         time.sleep(1)
