5e32d8725d2c364d486e52d681dd4affa88a5005
J <jimfinoc@gmail.com>
2025-02-12 20:39:38 -0800

    centering

--- weatherRefresh.py
+++ weatherRefresh.py
@@ -8,14 +8,15 @@ if __name__ == "__main__":
     timeHack = time.time()
     prettyNow = time.strftime("%H:%M:%S %Z   \n%a %m %b %Y   ",time.localtime(time.time()))
     print(prettyNow)
+    print("\033[1;1H", end="")
     print()
     weather.print_weather()
 
     while True:
         time.sleep(1)
-        print("\033[1;1H", end="")
         prettyNow = time.strftime("%H:%M:%S %Z   \n%a %m %b %Y   ",time.localtime(time.time()))
         print(prettyNow)
+        print("\033[1;1H", end="")
         if time.time() - timeHack > 180:
             print(prettyNow)
             os.system('clear')
