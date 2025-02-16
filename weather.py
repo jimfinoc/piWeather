28cba501335b340c5f1182e55fbb31292226df63
J <jimfinoc@gmail.com>
2025-02-15 21:03:59 -0800

    fall over works!

--- weather.py
+++ weather.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 import json
 import gps
 import time
+import os
+import redis
 
 def get_weather_url(lat, lon):
     return f"https://api.weather.gov/points/{lat},{lon}"
@@ -17,7 +19,7 @@ def print_weather():
         lat = 'n/a'
         lon = 'n/a'
         try:
-            print('try gps.GPS()')
+            print('-try gps.GPS()')
             myGPS = gps.GPS()
             lat = myGPS.lat()
             lon = myGPS.lon()
@@ -25,9 +27,10 @@ def print_weather():
             print (f"Lat: {lat}, Lon: {lon}")
             print ("gps data loaded from function")
         except:
-            print("error getting database from the gps source")
+            print("=error getting database from the gps source")
+        if lat == 'n/a' and lon == 'n/a':
             try:
-                print('try loading file .My_location.json')
+                print('-try loading file .My_location.json')
                 filename = ".My_location.json"
                 with open(filename, "r") as file:
                     My_location = json.load(file)
@@ -35,17 +38,26 @@ def print_weather():
                 lon = My_location['Lon']
                 print ("gps data loaded from file")
             except:
-                print ("error getting data from file")
-                try:
-                    print('redis database')               
-                    r = redis.Redis(db=0,host=os.getenv("redis_database_name"),port=os.getenv("redis_database_port"),password=os.getenv("redis_database_password"))
-                
-                    My_location_json_string = r.get('My_location')
-                    My_location = json.loads(My_location_json_string)
-                    lat = My_location['Lat']
-                    lon = My_location['Lon']
-                except:
-                    print ("error getting data from database")
+                print ("=error getting data from file")
+        if lat == 'n/a' and lon == 'n/a':
+            try:
+                print('-try redis database')
+                load_dotenv(".env")
+                print ("redis_database_name")
+                print (" ",os.getenv("redis_database_name"))
+                print ("redis_database_port")
+                print (" ",os.getenv("redis_database_port"))
+                print ("redis_database_password")
+                print (" ",os.getenv("redis_database_password"))
+                r = redis.Redis(db=0,host=os.getenv("redis_database_name"),port=os.getenv("redis_database_port"),password=os.getenv("redis_database_password"))
+                My_location_json_string = r.get('My_location')
+                My_location = json.loads(My_location_json_string)
+                print("My_location")
+                print(My_location)
+                lat = My_location['Lat']
+                lon = My_location['Lon']
+            except:
+                print ("=error getting data from database")
         if lat != 'n/a' and lon != 'n/a':
             print ("Lat:",lat)
             print ("Lon:",lon)
