f2ec5265a79eeae5a1430666ee95762a37e7d73e
J <jimfinoc@gmail.com>
2025-02-17 19:35:33 -0800

    datetime is just time and date

---
+++ tkDateTime.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+import time
+
+def update_time():
+    current_time = time.strftime("%H:%M:%S %Z \n %a %d %b %Y")
+    label.config(text=current_time)
+    label.after(1000, update_time)
+
+window = tk.Tk()
+window.geometry("375x300")
+window.title("Digital Clock")
+
+label = tk.Label(window, font=("Arial", 30), text="00:00:00")
+label.grid(column=0,row=0)
+window.grid_columnconfigure((0), weight=1)
+window.grid_rowconfigure((0), weight=1)
+
+update_time()
+
+window.mainloop()
