6df93f9eda79eae7e039fc21ed3c64a57b5138c9
J <jimfinoc@gmail.com>
2025-02-18 09:22:00 -0800

    may have solved a memory errory of drawing too much and not updating it

--- tkClockAnalog.py
+++ tkClockAnalog.py
@@ -15,16 +15,37 @@ window.title("Analog Clock")
 canvas = tk.Canvas(window, width=x_geometry, height=y_geometry)
 canvas.pack()
 refresh = 1000
-def update_time():
-    cx0 = padx # x coordinate of the top-left corner of the bounding box
-    cy0 = pady  # y coordinate of the top-left corner of the bounding box
-    cx1 = x_geometry - padx  # x coordinate of the bottom-right corner of the bounding box
-    cy1 = y_geometry - pady # y coordinate of the bottom-right corner of the bounding box
 
+minuteAngle = 0
+secondAngle = 0
+hourAngle = 0
+
+
+cx0 = padx # x coordinate of the top-left corner of the bounding box
+cy0 = pady  # y coordinate of the top-left corner of the bounding box
+cx1 = x_geometry - padx  # x coordinate of the bottom-right corner of the bounding box
+cy1 = y_geometry - pady # y coordinate of the bottom-right corner of the bounding box
+lx0 = (x_geometry)/2  # Starting coordinates
+ly0 = (y_geometry)/2  # Starting coordinates
+
+mx1 = cx0
+my1 = cy0
+sx1 = cx0
+sy1 = cy0
+hx1 = cx0
+hy1 = cy0
+
+radius = 7.5
+bigCircle = canvas.create_oval(cx0, cy0, cx1, cy1, outline="white", fill="black")
+Start = canvas.create_oval(lx0-radius, ly0-radius, lx0+radius, ly0+radius, outline="white", fill="white")
+hourLine = canvas.create_line(lx0, ly0, hx1, hy1, width=radius*2, fill="white")
+minuteLine = canvas.create_line(lx0, ly0, mx1, my1, width=radius, fill="white")
+secondLine = canvas.create_line(lx0, ly0, sx1, sy1, width=radius/3, fill="red")
+
+
+def update_time():
     # lx0 = (cx1 - cx0)/2  # Starting coordinates
-    lx0 = (x_geometry)/2  # Starting coordinates
     # ly0 = (cy1 - cy0)/2  # Starting coordinates
-    ly0 = (y_geometry)/2  # Starting coordinates
 
     now = datetime.datetime.now()
     # print("now",now)
@@ -47,12 +68,19 @@ def update_time():
 
 
 
-    bigCircle = canvas.create_oval(cx0, cy0, cx1, cy1, outline="white", fill="black")
-    radius = 7.5
-    Start = canvas.create_oval(lx0-radius, ly0-radius, lx0+radius, ly0+radius, outline="white", fill="white")
-    hourLine = canvas.create_line(lx0, ly0, hx1, hy1, width=radius*2, fill="white")
-    minuteLine = canvas.create_line(lx0, ly0, mx1, my1, width=radius, fill="white")
-    secondLine = canvas.create_line(lx0, ly0, sx1, sy1, width=radius/3, fill="red")
+    # Start
+    # hourLine
+    # minuteLine
+    # secondLine.config()
+    canvas.coords(bigCircle, cx0, cy0, cx1, cy1)
+    # bigCircle = canvas.create_oval(cx0, cy0, cx1, cy1, outline="white", fill="black")
+    # Start = canvas.create_oval(lx0-radius, ly0-radius, lx0+radius, ly0+radius, outline="white", fill="white")
+    canvas.coords(hourLine,lx0, ly0, hx1, hy1)
+    # hourLine = canvas.create_line(lx0, ly0, hx1, hy1, width=radius*2, fill="white")
+    canvas.coords(minuteLine,lx0, ly0, mx1, my1)
+    # minuteLine = canvas.create_line(lx0, ly0, mx1, my1, width=radius, fill="white")
+    canvas.coords(secondLine,lx0, ly0, sx1, sy1)
+    # secondLine = canvas.create_line(lx0, ly0, sx1, sy1, width=radius/3, fill="red")
     # minuteEnd = canvas.create_oval(mx1-radius/3, my1-radius/3, mx1+radius/3, my1+radius/3, outline="white", fill="white")
     # hourEnd = canvas.create_oval(hx1-radius, hy1-radius, hx1+radius, hy1+radius, outline="white", fill="white")
     # hourLine.after(15,update_time)
@@ -64,5 +92,6 @@ def update_time():
 # label.pack()
 
 # update_time()
+update_time()
 window.after(refresh,update_time)
 window.mainloop()
